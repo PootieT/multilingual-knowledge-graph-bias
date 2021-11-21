35178096f4a9b4e11298f3f508d76db1efd19efa
Zilu Tang <zilu.p.tang@gmail.com>
2021-11-21 09:10:41 -0500

    fix bug keep gender terms, change filter by head count only (not combind head+tail), filter out people entity

--- data_exploration.py
+++ data_exploration.py
@@ -3,15 +3,74 @@ import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
+GENDER_SF = [
+    [
+        "male@en",
+        "Male@en",
+        "M@en",
+        "m@en",
+        "Boy@en",
+        "Boys@en",
+        "Males@en",
+    ],
+    [
+        "f@en",
+        "female@en",
+        "w@en",
+        "Female@en",
+        "F@en",
+        "women@en",
+        "Girls@en",
+        "Girl@en",
+        "W@en",
+    ],
+]
 
-def drop_least_frequent_entities(df: pd.DataFrame, frac: float) -> pd.DataFrame:
+
+def drop_least_frequent_entities(
+    df: pd.DataFrame,
+    frac: float,
+    keep_gender=True,
+    keep_profession=True,
+    keep_person=True,
+) -> pd.DataFrame:
+    if keep_gender:
+        gender_idx = df["relation"] == "<http://dbpedia.org/property/gender>"
+        df_gender = df[gender_idx]
+        df = df[~gender_idx]
+    if keep_profession:
+        prof_idx = df["relation"].isin(
+            [
+                "<http://dbpedia.org/property/profession>",
+                "<http://dbpedia.org/ontology/occupation>",
+                "<http://dbpedia.org/property/occupation>",
+                "<http://purl.org/linguistics/gold/hypernym>",  # TODO this pertains to other things as well but preserves to hierarchical structure
+            ]
+        )
+        df_prof = df[prof_idx]
+        df = df[~prof_idx]
+    if keep_person:
+        with open("data/dbpedia/en/person_with_gender.ent") as f:
+            person_list = [l.strip() for l in f.readlines()]
+        ppl_idx = df["head"].isin(person_list)
+        df_ppl = df[ppl_idx]
+        df = df[~ppl_idx]
+
     df["head_count"] = df["head"].map(df["head"].value_counts())
-    df["tail_count"] = df["tail"].map(df["tail"].value_counts())
+    # df["tail_count"] = df["tail"].map(df["tail"].value_counts())
     # add log so distribution can be more even, otherwise most will be dominated by popular entities
-    df["count"] = df["head_count"] + df["tail_count"]
-    df = df.drop(columns=["head_count", "tail_count"])
-    df = df.sample(weights=df["count"], frac=frac)
-    df = df.drop(columns=["count"])
+    # df["count"] = df["head_count"] + df["tail_count"]
+
+    # df = df.drop(columns=["head_count", "tail_count"])
+    df = df.sample(weights=df["head_count"], frac=frac)
+    df = df.drop(columns=["head_count"])
+
+    if keep_gender:
+        df = df.append(df_gender)
+    if keep_profession:
+        df = df.append(df_prof)
+    if keep_person:
+        df = df.append(df_ppl)
     return df
 
 
@@ -33,14 +92,7 @@ def import_data(
     )
 
     # remove some hard to parse ones (usually will be filtered out later too)
-    orig_len = len(df)
-    df = df[df["parse_error"] == "."]
-    df = df.drop(columns=["parse_error"])
-    drop_len = len(df)
-    diff_len = orig_len - drop_len
-    print(
-        f"Dropped {diff_len} ({(diff_len/orig_len):.4f}) of the incorrectly parsed rows. Now data length = {drop_len}"
-    )
+    df = remove_parse_errors(df)
 
     # filter triples so only ones left are the ones with tails also being a resource (not some surface form/facts)
     dbpedia_str = (
@@ -49,6 +101,12 @@ def import_data(
         else f"<http://{language}.dbpedia.org/resource/"
     )
     orig_len = len(df)
+
+    df.loc[df["tail"].isin(GENDER_SF[0]), "tail"] = "<http://dbpedia.org/resource/Male>"
+    df.loc[
+        df["tail"].isin(GENDER_SF[1]), "tail"
+    ] = "<http://dbpedia.org/resource/Female>"
+
     if keep_literals:
         # avoid filtering out date, year, literals that NELL dataset also include
         mask = df["tail"].str.contains(dbpedia_str) | ~df["tail"].str.contains(
@@ -87,6 +145,18 @@ def import_data(
         f"{language} dbpedia data created: \n"
         f"Train: {len(train)}, valid: {len(valid)}, test: {len(test)}"
     )
+
+
+def remove_parse_errors(df):
+    orig_len = len(df)
+    df = df[df["parse_error"] == "."]
+    df = df.drop(columns=["parse_error"])
+    drop_len = len(df)
+    diff_len = orig_len - drop_len
+    print(
+        f"Dropped {diff_len} ({(diff_len / orig_len):.4f}) of the incorrectly parsed rows. Now data length = {drop_len}"
+    )
+    return df
 
 
 if __name__ == "__main__":
